5e9d16c6c2f973f8551e98c40c86b070ffc775bf
GribanovIvan <van.a.gribanov@gmail.com>
2025-09-06 16:35:34 +0300

    beautify

--- main.cpp
+++ main.cpp
@@ -5,82 +5,79 @@
 
 enum SIDE {CLIENT,SERVER,BOTH};
 int main(int argc, char* argv[]) {
-        if (argc < 2 ) {
-						std::cout << "Usage: " << argv[0] << " <path_to_mods_directory>" << std::endl;
-						return 2;
-					}
+	if (argc < 2 ) {
+	std::cout << "Usage: " << argv[0] << " <path_to_mods_directory>" << std::endl;
+	return 2;
+		}
+	bool needcurse=1;
 	std::vector<unsigned int> curse;
 	std::vector<std::string> url;
 	std::vector<char> urlsides;
-        for (const auto& entry : std::filesystem::directory_iterator(argv[1])) {
-								if (entry.path().extension() != ".toml") {
-										std::cout << "Found non-toml file: " << entry.path().filename() << " Ignoring..." << std::endl;
-								}
-								else {
+	for (const auto& entry : std::filesystem::directory_iterator(argv[1])) {
+		if (entry.path().extension() != ".toml") {
+		std::cout << "Found non-toml file: " << entry.path().filename() << " Ignoring..." << std::endl;
+			} else {
 #ifdef DEBUG
-										std::cerr << entry.path().filename() << std::endl;
+		std::cerr << entry.path().filename() << std::endl;
 #endif
-										std::ifstream file(entry.path());
-										std::string line;
-										char side=BOTH;
-										while(true)
-										if(std::getline(file, line)) {
-												size_t pos = line.find("side");
-												if (pos != std::string::npos) {
-														line=line.substr(line.find("=")+1);
-														while (line[0]==' '||line[0]=='"') line=line.substr(1);
-														while (line[line.size()-1]==' '||line[line.size()-1]=='"') line=line.substr(0,line.size()-1);
-														if (line=="client") side=CLIENT;
-														else if (line=="server") side=SERVER;
-														else if (line!="both")std::cerr << "Unknown side: " << line << " in file: " << entry.path().filename() << std::endl;
-														break;
-												}
-											}
-										else {
-												std::cerr<<"Side not found, assuming \"both\": "<<entry.path().filename()<<std::endl;
-												file.clear();
-												file.seekg(0);
-												break;
-										}
-										while(std::getline(file,line)){
-											if (line.substr(0,3)=="url")
-												{
-													line=line.substr(line.find("=")+1);
-													while (line[0]==' '||line[0]=='"') line=line.substr(1);
-													while (line[line.size()-1]=='"') line=line.substr(0,line.size()-1);
-													url.push_back(line);
-													urlsides.push_back(side);
-													break;
-												}
-											if (line=="[update.curseforge]") getline(file,line);else continue;
-											curse.push_back(std::stoi(line.substr(line.find("=")+1)));
-											getline(file,line);
-											curse.push_back(std::stoi(line.substr(line.find("=")+1)));
-											curse.push_back(side);
-											break;
-											}
-										if(!line.size()) std::cerr<<"No url or curseforge section found in file: "<<entry.path().filename()<<" - skipping" <<std::endl;
-										file.close();
-								}
-        }
-std::cout<<"{\n\t\"curse\":\n\t[";
-unsigned short count = 0;
-while (count<curse.size()/3){
-	std::cout<<"\n\t\t{\n\t\t\t\"addonId\": "<<curse[count*3+1]<<",\n\t\t\t\"fileId\": "<<curse[count*3];
+		std::ifstream file(entry.path());
+		std::string line;
+		char side=BOTH;
+		while(needcurse)
+		if(std::getline(file, line)) {
+			size_t pos = line.find("side");
+			if (pos != std::string::npos) {
+				line=line.substr(line.find("=")+1);
+				while (line[0]==' '||line[0]=='"') line=line.substr(1);
+				while (line[line.size()-1]==' '||line[line.size()-1]=='"') line=line.substr(0,line.size()-1);
+				if (line=="client") side=CLIENT;
+				else if (line=="server") side=SERVER;
+				else if (line!="both")std::cerr << "Unknown side: " << line << " in file: " << entry.path().filename() << std::endl;
+				break;
+				}
+			}	else {
+				std::cerr<<"Side not found, assuming \"both\": "<<entry.path().filename()<<std::endl;
+				file.clear();file.seekg(0);
+				break;
+			}
+		while(std::getline(file,line)) {
+			if (line.substr(0,3)=="url") {
+				line=line.substr(line.find("=")+1);
+				while (line[0]==' '||line[0]=='"') line=line.substr(1);
+				while (line[line.size()-1]=='"') line=line.substr(0,line.size()-1);
+				url.push_back(line);
+				urlsides.push_back(side);
+				break;
+			}
+			if (line=="[update.curseforge]") getline(file,line);else continue;
+			curse.push_back(std::stoi(line.substr(line.find("=")+1)));
+			getline(file,line);
+			curse.push_back(std::stoi(line.substr(line.find("=")+1)));
+			curse.push_back(side);
+			break;
+			}
+		if(!line.size()) std::cerr<<"No url or curseforge section found in file: "<<entry.path().filename()<<" - skipping" <<std::endl;
+		file.close();
+			}
+		}
+	std::cout<<"{\n\t\"curse\":\n\t[";
+	unsigned short count = 0;
+	while (count<curse.size()/3) {
+		std::cout<<"\n\t\t{\n\t\t\t\"addonId\": "<<curse[count*3+1]<<",\n\t\t\t\"fileId\": "<<curse[count*3];
 		if (curse[count*3+2] != BOTH) std::cout<<",\n\t\t\t\"metadata\": {\n\t\t\t\t\"side\": \""<<(curse[count*3+2]==CLIENT?"CLIENT":(curse[count*3+2]==SERVER?"SERVER":"BOTH"))<<"\"\n\t\t\t}";
-	std::cout<<"\n\t\t}";
+		std::cout<<"\n\t\t}";
 		if (count<curse.size()/3-1) std::cout<<",";
-	count++;
-}
-std::cout<<"\n\t],\n\t\"url\":\n\t[";
-count=0;
-while (count<url.size()){
-	std::cout<<"\n\t\t{\n\t\t\t\"url\": \""<<url[count]<<"\"";
+		count++;
+		}
+	std::cout<<"\n\t],\n\t\"url\":\n\t[";
+	count=0;
+	while (count<url.size()){
+		std::cout<<"\n\t\t{\n\t\t\t\"url\": \""<<url[count]<<"\"";
 		if (urlsides[count] != BOTH) std::cout<<",\n\t\t\t\"metadata\": {\n\t\t\t\t\"side\": \""<<(urlsides[count]==CLIENT?"CLIENT":(urlsides[count]==SERVER?"SERVER":"BOTH"))<<"\"\n\t\t\t}";
-	std::cout<<"\n\t\t}";
-	if (count<url.size()-1) std::cout<<",";
-	count++;
-}
-std::cout<<"\n\t]\n}\n";
-    return 0;
+		std::cout<<"\n\t\t}";
+		if (count<url.size()-1) std::cout<<",";
+		count++;
+	}
+	std::cout<<"\n\t]\n}\n";
+	return 0;
 }
